f5a70acddafbe6bfb45c7834a4e044cb28136682
Steve Tucker <stevet@solidstatenetworks.com>
2023-03-29 11:07:52 -0700

    Basic linter fixes.

--- jira.py
+++ jira.py
@@ -1,35 +1,38 @@
 import argparse
 import json
-import time
 import sys
+import time
 
 import requests
 
-parser = argparse.ArgumentParser(description='Jira releases')
-parser.add_argument('--email', help='E-mail address', action='store', required=True)
-parser.add_argument('--api_token', help='Api token', action='store', required=True)
-parser.add_argument('--project_key', help='Project key', action='store', required=True)
-parser.add_argument('--host', help='Jira hostname', action='store', required=True)
-parser.add_argument('--version', help='Version name', action='store', required=True)
-parser.add_argument('--release', help='Release', action='store_true', required=False)
-parser.add_argument('--delete', help='Delete', action='store_true', required=False)
+parser = argparse.ArgumentParser(description="Jira releases")
+parser.add_argument("--email", help="E-mail address", action="store", required=True)
+parser.add_argument("--api_token", help="Api token", action="store", required=True)
+parser.add_argument("--project_key", help="Project key", action="store", required=True)
+parser.add_argument("--host", help="Jira hostname", action="store", required=True)
+parser.add_argument("--version", help="Version name", action="store", required=True)
+parser.add_argument("--release", help="Release", action="store_true", required=False)
+parser.add_argument("--delete", help="Delete", action="store_true", required=False)
 args, _ = parser.parse_known_args()
 
 
 # Retrieve the id of a project with its key
 def jira_version_get_project_id(project_key):
-    response = requests.get(url=f"https://{args.host}/rest/api/3/project/{project_key}?properties=id",
-                            auth=(args.email, args.api_token),
-                            headers={"Content-Type": "application/json"})
+    response = requests.get(
+        url=f"https://{args.host}/rest/api/3/project/{project_key}?properties=id",
+        auth=(args.email, args.api_token),
+        headers={"Content-Type": "application/json"},
+        timeout=60,
+    )
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err.response.text)
         raise
 
-    json = response.json()
-    if 'id' in json:
-        return int(json['id'])
+    response_json = response.json()
+    if "id" in response_json:
+        return int(response_json["id"])
 
     return None
 
@@ -37,9 +40,12 @@ def jira_version_get_project_id(project_key):
 # Get a project version
 def jira_version_get(project_id, name):
     # Missing key errors can also be caused by missing user permissions
-    response = requests.get(url=f"https://{args.host}/rest/api/3/project/{project_id}/versions",
-                            auth=(args.email, args.api_token),
-                            headers={"Content-Type": "application/json"})
+    response = requests.get(
+        url=f"https://{args.host}/rest/api/3/project/{project_id}/versions",
+        auth=(args.email, args.api_token),
+        headers={"Content-Type": "application/json"},
+        timeout=60,
+    )
 
     try:
         response.raise_for_status()
@@ -56,17 +62,15 @@ def jira_version_get(project_id, name):
 
 # Add a project version
 def jira_version_add(project_id, name):
-    params = {
-        "name": name,
-        "archived": False,
-        "projectId": project_id,
-        "startDate": time.strftime("%Y-%m-%d")
-    }
+    params = {"name": name, "archived": False, "projectId": project_id, "startDate": time.strftime("%Y-%m-%d")}
     print(params)
-    response = requests.post(url=f"https://{args.host}/rest/api/3/version",
-                             auth=(args.email, args.api_token),
-                             headers={"Content-Type": "application/json"},
-                             data=json.dumps(params))
+    response = requests.post(
+        url=f"https://{args.host}/rest/api/3/version",
+        auth=(args.email, args.api_token),
+        headers={"Content-Type": "application/json"},
+        data=json.dumps(params),
+        timeout=60,
+    )
 
     try:
         response.raise_for_status()
@@ -81,9 +85,12 @@ def jira_version_add(project_id, name):
 
 # Delete a project version
 def jira_version_delete(version):
-    response = requests.delete(url=f"https://{args.host}/rest/api/3/version/{version['id']}",
-                               auth=(args.email, args.api_token),
-                               headers={"Content-Type": "application/json"})
+    response = requests.delete(
+        url=f"https://{args.host}/rest/api/3/version/{version['id']}",
+        auth=(args.email, args.api_token),
+        headers={"Content-Type": "application/json"},
+        timeout=60,
+    )
 
     try:
         response.raise_for_status()
@@ -94,23 +101,25 @@ def jira_version_delete(version):
 
 # Release an existing project version
 def jira_version_release(version):
-    version['released'] = True
+    version["released"] = True
 
     # Can't have startDate and userStartDate
-    if 'startDate' in version and 'userStartDate' in version:
-        del version['startDate']
+    if "startDate" in version and "userStartDate" in version:
+        del version["startDate"]
 
     # Can't have releaseDate and userReleaseDate
-    if 'userReleaseDate' in version:
-        version['userReleaseDate'] = time.strftime("%d/%b/%Y")
+    if "userReleaseDate" in version:
+        version["userReleaseDate"] = time.strftime("%d/%b/%Y")
     else:
-        version['releaseDate'] = time.strftime("%Y-%m-%d")
+        version["releaseDate"] = time.strftime("%Y-%m-%d")
 
-    response = requests.put(url=f"https://{args.host}/rest/api/3/version/{version['id']}",
-                            auth=(args.email, args.api_token),
-                            headers={
-                                "Content-Type": "application/json"
-                            }, data=json.dumps(version))
+    response = requests.put(
+        url=f"https://{args.host}/rest/api/3/version/{version['id']}",
+        auth=(args.email, args.api_token),
+        headers={"Content-Type": "application/json"},
+        data=json.dumps(version),
+        timeout=60,
+    )
 
     try:
         response.raise_for_status()
@@ -124,7 +133,7 @@ def jira_version_release(version):
 def main():
     project_id = jira_version_get_project_id(args.project_key)
     if not project_id:
-        raise Exception("Project does not exist")
+        raise RuntimeError("Project does not exist")
 
     version = jira_version_get(project_id, args.version)
     if not version:
@@ -143,11 +152,11 @@ def main():
 
         if args.release:
             print("Releasing version")
-            if version['released']:
+            if version["released"]:
                 print("Version already released")
             else:
                 jira_version_release(version)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
